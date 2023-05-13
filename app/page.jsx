65335e3da6d1d17bd8a6007dc867911ff7554a9c
Paul Schofield <paul.schofield.us@gmail.com>
2023-05-13 10:28:54 -0400

    layout, component initialization and styling

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import TaskFeed from '@components/TaskFeed';
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <TaskFeed />
+    </div>
+  );
+};
+
+export default Home;
---
+++ components/TaskFeed.jsx
@@ -0,0 +1,5 @@
+const TaskFeed = () => {
+  return <div>TaskFeed</div>;
+};
+
+export default TaskFeed;
