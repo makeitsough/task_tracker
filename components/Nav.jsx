07aa6e1f9bba3a66e223565ed4eda889e2711656
Paul Schofield <paul.schofield.us@gmail.com>
2023-05-13 19:06:32 -0400

    home page and icon

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,25 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  const isLoggedIn = true;
+  return (
+    <nav className="flex-between w-full mb-14 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/icons/cloud.png"
+          alt="logo"
+          width={40}
+          height={340}
+          className="object-contain"
+        ></Image>
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
